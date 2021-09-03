efdc641b525963fed2da7ccb115784421492b462
Dilya <dildoranasriddinzoda07@gmail.com>
2021-09-03 12:45:15 +0300

    first commit

--- 1/Program.cs
+++ 1/Program.cs
@@ -6,64 +6,64 @@ namespace _24._08._2021
     {
         static void Main(string[] args)
         {
-           string a2 = "NuN";
+           string n2 = "NuN";
             Console.WriteLine("Выберите валюту usd, euro, rub, som");
-            string a1 = Console.ReadLine();
-            if (a1 == "som")
+            string n1 = Console.ReadLine();
+            if (n1 == "som")
             {
                 Console.WriteLine("Выберите вторую волюту usd, euro, rub");
-                a2 = Console.ReadLine();
+                n2 = Console.ReadLine();
             }
             Console.WriteLine("введите сумму конвертации");
             double s = Convert.ToDouble(Console.ReadLine());
                 Converter conver = new Converter()
-            {
-                m1 = a1,
+            { 
+                a1 = n1,
                 sum = s,
-                m2 = a2
+                a2 = n2
             };
             conver.GetConv();
         }
     }
     class Converter
     {
-        public string m1;
-        public string m2;
+        public string a1;
+        public string a2;
         public double convert;
         public double sum;
         public Converter() { }
-        public Converter(string M1, string M2, double Sum)
+        public Converter(string A1, string A2, double Sum)
         {
-           m1 = M1; m2 = M2; convert = Sum;
+           a1 = A1; a2 = A2; convert = Sum;
         }
         public void GetConv()
         {
-            if (m1 == "usd")
+            if (a1 == "usd")
             {
                 convert = sum * 1132.86 / 100;
                 Console.WriteLine($"{sum} dollar = {Math.Round(convert, 2)} somoni");
             }
-            else if (m1 == "som" && m2 == "usd")
+            else if (a1 == "som" && a2 == "usd")
             {
                 convert = sum * 8.74 / 100;
                 Console.WriteLine($"{sum} somoni = {Math.Round(convert, 2)} dollar");
             }
-            else if (m1 == "euro")
+            else if (a1 == "euro")
             {
                 convert = sum * 1345.52 / 100;
                 Console.WriteLine($"{sum} evro = {Math.Round(convert, 2)} somoni");
             }
-            else if (m1 == "som" && m2 == "euro")
+            else if (a1 == "som" && a2 == "euro")
             {
                 convert = sum * 7.44 / 100;
                 Console.WriteLine($"{sum} somoni = {Math.Round(convert, 2)} evro");
             }
-            else if (m1 == "rub")
+            else if (a1 == "rub")
             {
                 convert = sum * 15.45 / 100;
                 Console.WriteLine($"{sum} rubl = {Math.Round(convert, 2)} somoni");
             }
-            else if (m1 == "som" && m2 == "rub")
+            else if (a1 == "som" && a2 == "rub")
             {
                 convert = sum * 647.60 / 100;
                 Console.WriteLine($"{sum} somoni = {Math.Round(convert, 2)} rubl");
